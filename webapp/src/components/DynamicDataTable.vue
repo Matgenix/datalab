51c25b570bf3c1ca2e8e4fd0cc71309304d396ac
David Waroquiers <david.waroquiers@matgenix.com>
2026-07-15 11:55:48 +0200

    Reviewed changes.

--- webapp/src/components/DynamicDataTable.vue
+++ webapp/src/components/DynamicDataTable.vue
@@ -1,4 +1,3 @@
-<!-- This file was edited with the assistance of an AI model and requires human review from the contributor. -->
 <template>
   <div>
     <div v-if="isSampleFetchError" class="alert alert-danger">
